61f48c1679f5ce61e1d4f8c0fd020b8d1b180abb
Bradley <Bradley.lauweres@student.howest.be>
2025-03-31 14:03:19 +0200

    add: option to search by channel

--- src/downloader/clip_fetcher.py
+++ src/downloader/clip_fetcher.py
@@ -4,12 +4,20 @@ from src.utils.logger import log_info, log_error
 
 class ClipFetcher:
     BASE_URL = "https://api.twitch.tv/helix/clips"
+    BASE_URL_USERS = "https://api.twitch.tv/helix/users"
 
     def __init__(self):
         self.headers = {
             "Client-ID": "gs44smut15275gf87pqx9v0p9nqpi5",
             "Authorization": f"Bearer ea65d1wb7yw1w5n1pdjbzx84b3g9s8"
         }
+
+    def get_broadcastID(self):
+        broadcaster_name = input("Please enter the broadcaster name: ")
+        response = requests.get(self.BASE_URL_USERS, headers=self.headers, params={"login": broadcaster_name})
+        broadcaster_id = response.json().get("data", [{}])[0].get("id")
+        print(broadcaster_id)
+        return broadcaster_id
 
     def get_clips(self, game_id, clip_count):
         now = datetime.now().isoformat() + "Z"
@@ -21,7 +29,14 @@ class ClipFetcher:
         else:
             twenty_four_hours_ago = (datetime.now() - timedelta(hours=24)).isoformat() + "Z"
 
-        params = {"game_id": game_id, "started_at": twenty_four_hours_ago,"ended_at":now, "first": clip_count}
+        wantToEnterBroadcasterID = input("Do you want to enter a broadcaster ID? (y/n): ")
+      
+        if wantToEnterBroadcasterID == "y":
+            broadcast_id = self.get_broadcastID()
+            params = {"started_at": twenty_four_hours_ago, "ended_at": now, "first": clip_count, "broadcaster_id": broadcast_id}
+        else:
+            params = {"game_id": game_id, "started_at": twenty_four_hours_ago,"ended_at":now, "first": clip_count}
+        
         response = requests.get(self.BASE_URL, headers=self.headers, params=params)
         
         if response.status_code != 200:
@@ -32,3 +47,5 @@ class ClipFetcher:
         log_info(f"Fetched {len(clips)} clips.")
         
         return [{"url": clip["url"], "broadcaster_id": clip["broadcaster_id"]} for clip in clips]
+    
+    
