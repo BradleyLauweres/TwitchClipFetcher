afc4ea4f8a78ae58881aade264c50fbd812367c3
Bradley <Bradley.lauweres@student.howest.be>
2025-04-01 09:51:03 +0200

    cleanup: cleaning up bloated functions

--- src/downloader/clip_fetcher.py
+++ src/downloader/clip_fetcher.py
@@ -1,4 +1,5 @@
 import requests
+from dotenv import load_dotenv,dotenv_values
 from datetime import datetime, timedelta
 from src.utils.logger import log_info, log_error
 
@@ -7,9 +8,10 @@ class ClipFetcher:
     BASE_URL_USERS = "https://api.twitch.tv/helix/users"
 
     def __init__(self):
+        load_dotenv()
         self.headers = {
-            "Client-ID": "gs44smut15275gf87pqx9v0p9nqpi5",
-            "Authorization": f"Bearer ea65d1wb7yw1w5n1pdjbzx84b3g9s8"
+            "Client-ID": dotenv_values(".env")["CLIENT_ID"],
+            "Authorization": dotenv_values(".env")["ACCESS_TOKEN"],
         }
 
     def get_broadcastID(self):
@@ -18,9 +20,8 @@ class ClipFetcher:
         broadcaster_id = response.json().get("data", [{}])[0].get("id")
         print(broadcaster_id)
         return broadcaster_id
-
-    def get_clips(self, game_id, clip_count):
-        now = datetime.now().isoformat() + "Z"
+    
+    def change_base_time(self):
         wantToChangeBaseTime = input("Do you want to change the base time? (y/n): ")
 
         if wantToChangeBaseTime == "y":
@@ -29,15 +30,22 @@ class ClipFetcher:
             twenty_four_hours_ago = (datetime.now() - timedelta(hours=pickedDate)).isoformat() + "Z"
         else:
             twenty_four_hours_ago = (datetime.now() - timedelta(hours=24)).isoformat() + "Z"
-
+        return twenty_four_hours_ago
+        
+    def wantToEnterBroadcasterID(self,setTime , game_id, clip_count):
         wantToEnterBroadcasterID = input("Do you want to enter a broadcaster ID? (y/n): ")
+        now = datetime.now().isoformat() + "Z"
       
         if wantToEnterBroadcasterID == "y":
             broadcast_id = self.get_broadcastID()
-            params = {"started_at": twenty_four_hours_ago, "ended_at": now, "first": clip_count, "broadcaster_id": broadcast_id}
+            params = {"started_at": setTime, "ended_at": now, "first": clip_count, "broadcaster_id": broadcast_id}
         else:
-            params = {"game_id": game_id, "started_at": twenty_four_hours_ago,"ended_at":now, "first": clip_count}
-        
+            params = {"game_id": game_id, "started_at": setTime,"ended_at":now, "first": clip_count}
+        return params
+
+    def get_clips(self, game_id, clip_count):
+        setTime = self.change_base_time()
+        params = self.wantToEnterBroadcasterID(setTime, game_id, clip_count)
         response = requests.get(self.BASE_URL, headers=self.headers, params=params)
         
         if response.status_code != 200:
